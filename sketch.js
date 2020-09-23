bdbac10a71ac7782cdaa5e9b62bf6653a1b56010
benitavinod <71214293+benitavinod@users.noreply.github.com>
2020-09-23 18:10:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+var wall,thickness
+var bullet,speed,height
+
+function setup() {
+  createCanvas(1600,400);
+  speed=random(223,321);
+  weight=random(30,52);
+  thickness=random(22,83);
+  
+  bullet=createSprite(700,130,30,20);
+  bullet.velocityX=speed
+  bullet.shapeColor="white";
+  
+  wall=createSprite(1200,120,thickness,height/2)
+  wall.shapeColor=color(80,80,80);
+
+}
+
+function draw() {
+  background(0);  
+ 
+  if (hasCollided(bullet,wall)){
+    bullet.velocityX=0;
+    var damage=(0.5 * weight * speed * speed)/(thickness * thickness * thickness);
+    if(damage>10){
+      wall.shapeColor=color(255,0,0);
+    }
+    if (damage<10){
+      wall.shapeColor=color(0,255,0);
+    }
+
+  }
+  
+  drawSprites();
+}
+
+function hasCollided(bullet,wall){
+  bulletRightEdge=bullet.x + bullet.width;
+  wallLeftEdge = wall.x;
+  if (bulletRightEdge >= wallLeftEdge)
+  {
+    return true;
+  } 
+  return false;
+}
